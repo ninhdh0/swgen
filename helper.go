e95eff06863b41efddd64688b9703471aa62cd0a
Hoai Ninh <ninhdh0@gmail.com>
2017-08-12 09:51:39 +0700

    Generate ref name base on pkg path

--- helper.go
+++ helper.go
@@ -1,9 +1,13 @@
 package swgen
 
 import (
+	"bytes"
 	"fmt"
 	"reflect"
+	"regexp"
 )
+
+var camelingRegex = regexp.MustCompile("[0-9A-Za-z]+")
 
 // ReflectTypeHash returns private (unexported) `hash` field of the Golang internal reflect.rtype struct for a given reflect.Type
 // This hash is used to (quasi-)uniquely identify a reflect.Type value
@@ -15,6 +19,15 @@ func ReflectTypeHash(t reflect.Type) uint32 {
 // name for anonymous structs
 func ReflectTypeReliableName(t reflect.Type) string {
 	if t.Name() != "" {
+		byteSrc := []byte(t.PkgPath())
+		parts := camelingRegex.FindAll(byteSrc, -1)
+		if len(parts) > 2 {
+			chunks := parts[len(parts)-2 : len(parts)]
+			for idx, val := range chunks {
+				chunks[idx] = bytes.Title(val)
+			}
+			return string(bytes.Join(chunks, nil)) + t.Name()
+		}
 		return t.Name()
 	}
 	return fmt.Sprintf("anon_%08x", ReflectTypeHash(t))
